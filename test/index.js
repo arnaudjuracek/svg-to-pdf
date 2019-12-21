0b1134e78bf8efb6b83cb30b241f20e4948568b7
RNO <arnaud.juracek@gmail.com>
2019-12-21 23:38:05 +0100

    wip renderer and preflight

--- test/index.js
+++ test/index.js
@@ -1,20 +1,25 @@
-const SVG2PDF = require('..')
-const path = require('path')
 const fs = require('fs-extra')
-const args = process.argv.slice(2)
+const path = require('path')
+const SVG2PDF = require('..')
 
-const svgPath = args[0]
-  ? path.resolve(process.cwd(), args[0])
-  : path.join(__dirname, 'sample.svg')
-
-;(async function () {
-  try {
-    const svgString = await fs.readFile(svgPath, 'utf8')
-    console.log(svgString)
-    // TODO: handdle returning object like { result, warnings, etc… }
-    SVG2PDF(svgString)
-  } catch (err) {
-    console.error(err instanceof Error ? err : Error(err))
-    process.exit(1)
+const argv = require('minimist')(process.argv.slice(2), {
+  alias: { i: 'input', o: 'output' },
+  default: {
+    input: path.join(__dirname, 'sample.svg'),
+    output: undefined
   }
-})()
+})
+
+const options = {
+  docPath: argv.output
+}
+
+try {
+  const svgString = fs.readFileSync(argv.input, 'utf8')
+  const { warnings, docPath } = SVG2PDF(svgString, options)
+  console.log('⚠  Warning:', warnings)
+  console.log('✔  Success:', docPath)
+} catch (err) {
+  console.error(err instanceof Error ? err : Error(err))
+  process.exit(1)
+}
